e06a3d92bfa7b629ccdae79a8a2884f5169ad471
KhoaPham359 <phamminhkhoa255204@gmail.com>
2025-05-27 18:17:46 +0700

    Initial commit

---
+++ lua/plugins/user.lua
@@ -0,0 +1,32 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+return {
+  {
+    -- mapping
+    "AstroNvim/astrocore",
+    ---@type AstroCoreOpts
+    opts = {
+      mappings = {
+        n = {
+          -- line break
+          ["K"] = { "i<CR><Esc>", noremap = true, desc = "Insert line break" },
+          ["<leader>a"] = { "$a" },
+          ["<leader>i"] = { "0i" },
+          ["<tab>"] = { ">>" },
+          ["<S-tab>"] = { "<<" },
+        },
+        t = {
+          -- setting a mapping to false will disable it
+          -- ["<esc>"] = false,
+        },
+      },
+    },
+  },
+  {
+    -- colorscheme
+    "AstroNvim/astroui",
+    ---@type AstroUIOpts
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
